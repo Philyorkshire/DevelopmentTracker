e11a6bd655217c99731205ff513aa3d72b86f9fc
philyorkshire <p.marsden3653@student.leedsmet.ac.uk>
2014-11-20 14:33:53 +0000

    Upload blank .NET application
    Uploading blank .NET application to begin

---
+++ DevelopmentTracker/DevelopmentTracker/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevelopmentTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
